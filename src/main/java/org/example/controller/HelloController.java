db8466330990d10fcdb701f08c0fb34af5d6e06b
githubdezi <bitzdl@163.com>
2026-08-25 23:47:11 +0800

    first commit

---
+++ src/main/java/org/example/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Example REST controller.
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
+}
---
+++ src/test/java/org/example/TestMavenApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Application context smoke test.
+ */
+@SpringBootTest
+class TestMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
